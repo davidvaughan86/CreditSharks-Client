8348e40abe8b0e02fa1ae525c6192c1effabc3e3
davidvaughan86 <david.vaughan86@gmail.com>
2021-02-04 15:31:00 -0500

    adds hero page and header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+// import {useEffect, useState} from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useHistory
+} from 'react-router-dom'
+import Header from './Components/Header'
+import './HeroPage.css'
+import HeroPage from './Components/HeroPage';
+import AccessAlarmIcon from '@material-ui/icons/AccessAlarm';
+import ThreeDRotation from '@material-ui/icons/ThreeDRotation';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+
+        <Header />
+          <Switch>
+            <Route path ='/' exact>
+              <HeroPage />                
+                  
+            </Route>
+            
+
+
+
+
+
+
+          </Switch>
+          
+        
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Components/HeroPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+export default function HeroPage () {
+    return (
+        <div className='heroPageContainer' max-width = '80%'>
+            <div className="frame">
+
+                <h1>content</h1>
+
+
+            </div>
+        </div>
+    )
+}
